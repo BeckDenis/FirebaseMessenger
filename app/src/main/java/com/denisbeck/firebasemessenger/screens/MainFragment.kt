26591f8709132910069368ccd172087c697d9c6e
Denis <57283152+BeckDenis@users.noreply.github.com>
2020-07-07 11:11:52 +0600

    Added MaterialDrawer and NavigationComponent

---
+++ app/src/main/java/com/denisbeck/firebasemessenger/screens/MainFragment.kt
@@ -0,0 +1,8 @@
+package com.denisbeck.firebasemessenger.screens
+
+import androidx.fragment.app.Fragment
+import com.denisbeck.firebasemessenger.R
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+}
